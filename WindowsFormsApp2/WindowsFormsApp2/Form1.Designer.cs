dcf8700284f895526c9abe335f2d36791aafaacb
poikoak <87008326+poikoak@users.noreply.github.com>
2021-10-02 21:34:48 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yesTriangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noTriangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jPGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elipsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paintToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nopaintToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.brushToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treugolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // yesTriangleToolStripMenuItem
+            // 
+            this.yesTriangleToolStripMenuItem.Name = "yesTriangleToolStripMenuItem";
+            this.yesTriangleToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.yesTriangleToolStripMenuItem.Text = "закрашенные";
+            this.yesTriangleToolStripMenuItem.Click += new System.EventHandler(this.yesTriangleToolStripMenuItem_Click);
+            // 
+            // noTriangleToolStripMenuItem
+            // 
+            this.noTriangleToolStripMenuItem.Name = "noTriangleToolStripMenuItem";
+            this.noTriangleToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.noTriangleToolStripMenuItem.Text = "незакрашенные";
+            this.noTriangleToolStripMenuItem.Click += new System.EventHandler(this.noTriangleToolStripMenuItem_Click);
+            // 
+            // jPGToolStripMenuItem
+            // 
+            this.jPGToolStripMenuItem.Name = "jPGToolStripMenuItem";
+            this.jPGToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.jPGToolStripMenuItem.Text = "JPG";
+            this.jPGToolStripMenuItem.Click += new System.EventHandler(this.jPGToolStripMenuItem_Click);
+            // 
+            // projectToolStripMenuItem
+            // 
+            this.projectToolStripMenuItem.Name = "projectToolStripMenuItem";
+            this.projectToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.projectToolStripMenuItem.Text = "Project";
+            // 
+            // elipsToolStripMenuItem
+            // 
+            this.elipsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.paintToolStripMenuItem2,
+            this.nopaintToolStripMenuItem2});
+            this.elipsToolStripMenuItem.Name = "elipsToolStripMenuItem";
+            this.elipsToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.elipsToolStripMenuItem.Text = "элипс";
+            // 
+            // paintToolStripMenuItem2
+            // 
+            this.paintToolStripMenuItem2.Name = "paintToolStripMenuItem2";
+            this.paintToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.paintToolStripMenuItem2.Text = "закрашенные";
+            // 
+            // nopaintToolStripMenuItem2
+            // 
+            this.nopaintToolStripMenuItem2.Name = "nopaintToolStripMenuItem2";
+            this.nopaintToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.nopaintToolStripMenuItem2.Text = "незакрашенные";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jPGToolStripMenuItem,
+            this.projectToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.saveToolStripMenuItem.Text = "сохранить";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.brushToolStripMenuItem,
+            this.lainToolStripMenuItem,
+            this.treugolToolStripMenuItem,
+            this.elipsToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.colorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // brushToolStripMenuItem
+            // 
+            this.brushToolStripMenuItem.Name = "brushToolStripMenuItem";
+            this.brushToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.brushToolStripMenuItem.Text = "кисть";
+            this.brushToolStripMenuItem.Click += new System.EventHandler(this.brushToolStripMenuItem_Click);
+            // 
+            // lainToolStripMenuItem
+            // 
+            this.lainToolStripMenuItem.Name = "lainToolStripMenuItem";
+            this.lainToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.lainToolStripMenuItem.Text = "линий";
+            this.lainToolStripMenuItem.Click += new System.EventHandler(this.lainToolStripMenuItem_Click);
+            // 
+            // treugolToolStripMenuItem
+            // 
+            this.treugolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yesTriangleToolStripMenuItem,
+            this.noTriangleToolStripMenuItem});
+            this.treugolToolStripMenuItem.Name = "treugolToolStripMenuItem";
+            this.treugolToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.treugolToolStripMenuItem.Text = "треугольники";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.colorToolStripMenuItem.Text = "Цвет";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem brushToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem treugolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yesTriangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noTriangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elipsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paintToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem nopaintToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jPGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.IO;
+using System.Collections;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        class Figures
+        {
+            public Figures(int x1, int y1, int x2, int y2)
+            {
+                this.x1 = x1;
+                this.y1 = y1;
+                this.x2 = x2;
+                this.y2 = y2;
+            }
+
+            public int x1, y1, x2, y2;
+        }
+
+        int type = 1; //режим рисования
+        Point old = new Point();
+        bool flag = false;
+        ArrayList all = new ArrayList();
+        ArrayList current = new ArrayList();
+        Color Color = Color.Blue;
+        int baseSize = 5;
+        int x1, y1;
+
+
+
+        List<Figures> prim = new List<Figures>();
+        GraphicsState img;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Line(Graphics gr, MouseEventArgs e)
+        {
+            Pen pen = new Pen(Color, baseSize);
+
+            // задание формы концов линии
+            pen.SetLineCap(LineCap.Round, LineCap.Square, DashCap.Flat);
+
+            // Рисование линии
+            //gr.DrawLine(Pens.DarkMagenta, e.X, e.Y, x1, y1);
+
+            // Рисование линии
+            gr.DrawLine(pen, e.X, e.Y, x1, y1);
+
+            // Сохранение линии в списке
+            prim.Add(new Figures(e.X, e.Y, x1, y1));
+
+            // Поменять флаг
+            flag = false;
+        }
+
+        public void Brush(Graphics gr, MouseEventArgs e)
+        {
+            Pen pen = new Pen(Color, baseSize);
+            gr.DrawEllipse(pen, e.X, e.Y, 5, 5);
+            gr.Dispose();
+        }
+        public void FilledTriangle(Graphics gr, MouseEventArgs e)
+        {
+
+            Bitmap bitmap = new Bitmap(this.Width, this.Height);
+            bitmap.SetResolution(gr.DpiX, gr.DpiY);
+            using (Graphics G = Graphics.FromImage(bitmap))
+            {
+                Brush brush = new SolidBrush(Color);
+                Pen pen = new Pen(brush);
+                float xDiff = e.X - x1;
+                float yDiff = e.Y - y1;
+                float xMid = e.X + x1 / 2;
+                float yMid = e.Y + y1 / 2;
+
+
+                var path = new GraphicsPath();
+                path.AddLines(new PointF[] {e.Location,new PointF(xMid + yDiff/2, yMid-xDiff/2),new PointF(x1,y1)});
+                path.CloseFigure();               
+                gr.FillPath(brush, path);              
+                gr.DrawPath(pen, path);
+                gr.SmoothingMode = SmoothingMode.AntiAlias;
+                gr.CompositingQuality = CompositingQuality.HighQuality;
+                gr.CompositingMode = CompositingMode.SourceOver;
+                gr.DrawPath(new Pen(Color.Red), path);
+            }
+            bitmap.Save(@"TEST1.jpg", ImageFormat.Jpeg);
+        }
+
+
+        public void Triangle(Graphics gr, MouseEventArgs e)
+        {
+            Brush brush = new SolidBrush(Color);
+            Pen pen = new Pen(brush);
+            float xDiff = e.X - x1;
+            float yDiff = e.Y - y1;
+            float xMid = e.X + x1 / 2;
+            float yMid = e.Y + y1 / 2;            
+            var path = new GraphicsPath();
+            path.AddLines(new PointF[] { new PointF(e.X, e.Y), new PointF(xMid + yDiff / 2, yMid - xDiff / 2), new PointF(x1, y1)});
+            path.CloseFigure();
+            // рисуем треугол
+            gr.DrawPath(pen, path);
+        }
+
+
+        public void FilledRectangle(Graphics gr, MouseEventArgs e)
+        {
+            Brush brush = new SolidBrush(Color);
+            Pen pen = new Pen(brush);          
+            var path = new GraphicsPath();
+            path.AddLines(new PointF[] {new PointF(e.X, e.Y),new PointF(x1, e.Y),new PointF(x1,y1), new PointF(e.X, y1)});
+            path.CloseFigure();
+            gr.FillPath(brush, path);
+            gr.DrawPath(pen, path);
+        }
+
+        public void NotFilledRectangle(Graphics gr, MouseEventArgs e)
+        {
+            Brush brush = new SolidBrush(Color);
+            Pen pen = new Pen(brush);           
+            var path = new GraphicsPath();
+            path.AddLines(new PointF[] {new PointF(e.X, e.Y),new PointF(x1, e.Y),new PointF(x1,y1), new PointF(e.X, y1)});
+            path.CloseFigure();           
+            gr.DrawPath(pen, path);
+        }
+        private void lainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            type = 1;
+        }
+        private void brushToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            type = 2;
+        }
+        private void yesTriangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            type = 3;
+        }
+        private void yesRectToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            type = 5;
+        }
+        private void noTriangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            type = 4;
+        }
+        private void noRectToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            type = 6;
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Создание объекта для рисования в окне
+            Graphics gr = CreateGraphics();
+
+            if (!flag)
+            {
+                // Сохранить координаты первой точки
+                x1 = e.X;
+                y1 = e.Y;
+
+                // Поменять флаг
+                flag = true;
+            }
+            else
+            {
+                if (type == 1)
+                {
+                    Line(gr, e);
+                }
+                else if (type == 2)
+                {
+                    Brush(gr, e);
+                }
+                else if (type == 3)
+                {
+                    FilledTriangle(gr, e);
+                }
+                else if (type == 4) 
+                {
+                    Triangle(gr, e);
+                }
+                else if (type == 5)
+                {
+                    FilledRectangle(gr, e);
+                }
+                else if (type == 6) 
+                {
+                    NotFilledRectangle(gr, e);
+                }
+            }
+            img = gr.Save();         
+            gr.Dispose();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {           
+            Graphics gr = e.Graphics;          
+            foreach (Figures pr in prim)
+            {
+                Pen p = new Pen(Color.Blue, 3);
+                gr.DrawLine(p, pr.x1, pr.y1, pr.x2, pr.y2);
+            }
+        }
+
+       
+        private void jPGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var myForm = new Form();
+            myForm.StartPosition = FormStartPosition.Manual;
+            myForm.Location = new Point(500, 500);
+            myForm.Size = new Size(250, 250);
+            Button myButton = new Button();
+            myButton.Text = "Save";
+            myForm.Controls.Add(myButton);
+            TextBox filedir = new TextBox();
+            filedir.Text = @"TEST.jpg";
+            filedir.Location = new Point(100, 100);
+            myForm.Controls.Add(filedir);
+            myForm.Show();
+            if (myButton.Enabled)
+            {
+                filedir.Text = filedir.Text;               
+                Bitmap myBitmap = new Bitmap(this.Width, this.Height);
+                this.DrawToBitmap(myBitmap, new Rectangle(Point.Empty, myBitmap.Size));
+                myBitmap.Save(filedir.Text, ImageFormat.Jpeg);
+                   myForm.Close();
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.FullOpen = true;
+            dlg.ShowDialog();
+            Color = dlg.Color;
+        }
+
+
+    }
+
+}
