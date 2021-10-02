72db9d1b765e3f689134dfcc1e469a0a44a25c4b
poikoak <87008326+poikoak@users.noreply.github.com>
2021-10-02 21:37:14 +0300

    Add files via upload

---
+++ WindowsFormsLinkSQL/WindowsFormsApp1/Form4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form4 : Form
+    {
+        public string yesno;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yesno = "Yes";
+            if (yesno == "Yes")
+                this.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            yesno = "No";
+            this.Close();
+        }
+    }
+}
